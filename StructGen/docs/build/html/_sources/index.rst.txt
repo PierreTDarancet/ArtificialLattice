f34dba5339e943b1c2cd414b220bb18c59e84bad
Srilok <srilok1991@gmail.com>
2019-07-30 18:52:13 -0500

    Documentation update1 (incomplete)

--- StructGen/docs/build/html/_sources/index.rst.txt
+++ StructGen/docs/build/html/_sources/index.rst.txt
@@ -8,16 +8,13 @@ Welcome to TopoQuest's documentation!
 
 ``TopoQuest`` is a high throughput tight binding based python package the ``quest of topological materials`` with bound states. ``TopoQuest`` is built on ``kwant`` [REF] tight-binding package and ``Z2pack`` package from David Vanderbilt's group. ``TopoQuest`` samples the geometric space of structures satisfying the specified symmetry and geometric constrains as a graph. The graph is passed on to ``kwant`` which constructs the tight binding hamiltonian. The functions in ``Z2pack`` are used to compute the intercell Zak Phase (the origin independant but cell dependant part of Zak Phase) which characterises the topological invariant.    
 
-.. toctree::
-   :maxdepth: 2
-   :caption: Contents:
 
 
 Code documentation
 ==================
 
-.. automodule:: TopoQuest.StructGen
-    :members: 
+.. autoclass:: TopoQuest.StructGen.Check_redundant
+   :members:
 
 
 Indices and tables
--- StructGen/docs/source/conf.py
+++ StructGen/docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import sphinx_glpi_theme
 sys.path.insert(0, os.path.abspath('../../src'))
 
 
@@ -30,14 +31,24 @@ release = 'v0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon','sphinx.ext.autodoc']
+
+#extensions = ['numpydoc', 'sphinx.ext.todo',
+#    'sphinx.ext.githubpages',
+#                'sphinx.ext.autodoc']
+
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary',
+              'sphinx.ext.todo', 'sphinx.ext.mathjax', 'numpydoc']
+
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
+html_static_path = ['_static']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
+#pygments_style = 'sphinx'
 exclude_patterns = []
 
 
@@ -46,9 +57,10 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
+html_static_path = sphinx_glpi_theme.get_html_themes_path()
