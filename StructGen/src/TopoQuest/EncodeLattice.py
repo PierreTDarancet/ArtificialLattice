12800eba544e1cfdb130d0f219f03d22cfec1e77
Srilok <srilok1991@gmail.com>
2019-10-19 17:59:37 -0500

    Added encode method in EncodeLattice. TODO: Check and validate encode()

--- StructGen/src/TopoQuest/EncodeLattice.py
+++ StructGen/src/TopoQuest/EncodeLattice.py
@@ -7,6 +7,8 @@ Created on Fri Oct 18 13:13:23 2019
 """
 
 from TopoQuest.utilities import lattices 
+from itertools import combinations
+import numpy as np 
 import kwant 
 
 
@@ -16,8 +18,21 @@ class EncodeLattice():
         self.lat = lattices[lat]
         self.lx = self.lat.prim_vecs[0][0]
         self.ly = self.lat.prim_vecs[1][1]
-        self.UnitCellPosList = self.getUnitCellPos(self.lat)
+        self.UnitCell = self.getUnitCell(self.lat)
+        self.UnitCellPos = self._get_pos_list(self.UnitCell)
         self.center = [self.lx/2.0, self.ly/2.0]
+        self.encodeDict = self._get_encode_dict()
+        
+    def _get_encode_dict(self): 
+        encodeDict={}
+        vacancy_list = range(len(self.UnitCellPos)+1)
+        i=0
+        for v in vacancy_list:
+            for tile in combinations(self.UnitCellPos,v): 
+                encodeDict[i]=tile 
+                i += 1
+        return encodeDict
+        
         
     def _get_lat_center(lat): 
         prim_vecs = lat.prim_vecs
@@ -25,7 +40,7 @@ class EncodeLattice():
         yc = prim_vecs[1][1]/2.0 
         return [xc,yc]
     
-    def getUnitCellPos(self,lat): 
+    def getUnitCell(self,lat): 
         """Returns a list of positions of the unit cell for a given lattice
             (Currently works only for orthogonal unit cell) 
         """ 
@@ -39,24 +54,50 @@ class EncodeLattice():
         unitCell = kwant.Builder()
         unitCell[lat.shape((lambda pos: 0<=pos[0]<a and 0<=pos[1]<b),(0,0))] = 0 
         unitCell[lat.neighbors()]=-1 
-        return self._get_pos_list(unitCell)
+        return unitCell
     
     
     
-    def _get_pos_list(syst):
+    def _get_pos_list(self,syst):
         pos_list = []
         for site in syst.sites():
             pos_list.append(site.pos)
         return pos_list 
     
     
+    def _site_is_in(self,syst_pos,site): 
+        return np.isclose(syst_pos-site,[0,0]).all(axis=1).any()
+    
     def encode(self,syst):
         pos_list = np.array(self.get_pos_list(syst))
-        nlx = round(np.max(pos_list[:,0])/self.lx)
-        nly = round(np.max(pos_list[:,1]) - np.min(pos_list[:,1])/self.ly) + 1
+        nlx = int(round(np.max(pos_list[:,0])/self.lx))
+        nly = int(round(np.max(pos_list[:,1]) - np.min(pos_list[:,1])/self.ly) + 1)
+        systCode=[]
+        for ny in range(nly): 
+            rowCode=[]
+            for nx in range(nlx): 
+                xc = self.center +self.lx*nx 
+                yc = self.center +self.ly*ny 
+                shiftedLat = pos_list - [xc,yc] 
+                codeFound=False
+                for code,lat in self.encodeDict.items():
+                    if code!=0:
+                        for site in np.array(lat): 
+                            if not self._site_is_in(shiftedLat,site): 
+                                break 
+                            else: 
+                                tileCode=code
+                                codeFound=True 
+                if codeFound==False: 
+                    tileCode=0
+            rowCode.append(tileCode)
+        systCode.append(rowCode)
+        return np.array(systCode)
+                            
+                    
+                    
+                
+                
 
-        
-        
-        
         
 
