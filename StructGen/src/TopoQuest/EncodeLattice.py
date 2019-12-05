e188f9102f4ed5101ffa4760d0223005e8704cf7
Srilok <srilok1991@gmail.com>
2019-12-04 19:37:49 -0600

    Bugfix: wrong output of encode() is now fixed

--- StructGen/src/TopoQuest/EncodeLattice.py
+++ StructGen/src/TopoQuest/EncodeLattice.py
@@ -12,16 +12,13 @@ import numpy as np
 import kwant 
 from math import floor
 
-
 class EncodeLattice():
-
     def __init__(self,lat='Armchair',Nlx=10,Nly=10):
         self.lat = lattices[lat]
         self.lx = self.lat.prim_vecs[0][0]
         self.ly = self.lat.prim_vecs[1][1]
         self.UnitCell = self.getUnitCell(self.lat)
         self.UnitCellPos = self._get_pos_list(self.UnitCell)
-        self.center = [self.lx/2.0, self.ly/2.0]
         self.encodeDict = self._get_encode_dict()
         
     def _get_encode_dict(self): 
@@ -33,8 +30,7 @@ class EncodeLattice():
                 encodeDict[i]=tile 
                 i += 1
         return encodeDict
-        
-        
+           
     def _get_lat_center(lat): 
         prim_vecs = lat.prim_vecs
         xc = prim_vecs[0][0]/2.0 
@@ -45,7 +41,6 @@ class EncodeLattice():
         """Returns a list of positions of the unit cell for a given lattice
             (Currently works only for orthogonal unit cell) 
         """ 
-        
         # TODO: Works only for orthogonal unit cell. 
         # Find solutions for triclinic ones 
         prim_vecs = lat.prim_vecs
@@ -56,7 +51,6 @@ class EncodeLattice():
         unitCell[lat.shape((lambda pos: 0<=pos[0]<a and 0<=pos[1]<b),(0,0))] = 0 
         unitCell[lat.neighbors()]=-1 
         return unitCell
-    
     
     def _get_pos_list(self,syst):
         pos_list = []
@@ -76,29 +70,33 @@ class EncodeLattice():
             shiftVector = floor(min_y/self.ly)*self.ly
             pos_list[:,1] -= shiftVector
         nlx = int(round(np.max(pos_list[:,0])/self.lx))
-       # nly = int(round(np.max(pos_list[:,1]) - np.min(pos_list[:,1])/self.ly) + 1)
         nly = 20  
         systCode=[]
+
         for ny in range(nly): 
             rowCode=[]
             for nx in range(nlx): 
-                xc = self.center[0] +self.lx*nx 
-                yc = self.center[1] +self.ly*ny 
-                shiftedLat = pos_list - [xc,yc] 
+                x0 = self.lx*nx 
+                y0 = self.ly*ny 
+                shiftedLat = pos_list - [x0,y0]
+                mask = (0<=shiftedLat[:,0]) & (shiftedLat[:,0]<self.lx) & \
+                        (0<=shiftedLat[:,1]) & (shiftedLat[:,1]<self.ly-0.05) # 0.05 is to ensure the condition holds true 
+                                                                              # even when there are numerical errors 
+                CompareUnit = shiftedLat[mask,:]
                 codeFound=False
                 for code,lat in self.encodeDict.items():
-                    if code!=0:
-                        for site in np.array(lat): 
-                            if not self._site_is_in(shiftedLat,site): 
-                                break 
-                            else: 
-                                tileCode=code
-                                codeFound=True 
+                    latArray = np.array(lat)
+                    if latArray.shape == CompareUnit.shape:
+                        boolArray = []
+                        for site in latArray: 
+                            boolArray.append(self._site_is_in(CompareUnit,site))
+                        if np.all(np.array(boolArray)): 
+                            tileCode = code 
+                            codeFound = True 
                 if codeFound==False: 
                     tileCode=0
                 rowCode.append(tileCode)
             systCode.append(rowCode)
-            
         return np.flip(np.array(systCode),axis=0)
                             
                     
